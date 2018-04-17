02cf19ae1dba029cc726c6c2dd46fd4581ad6c8f
Nickolas Fuentes <nickfuentes24@gmail.com>
2018-04-16 20:09:33 -0500

    Testing completed, moving code back to original code

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
   render() {
     return (
-    
+      <form className="input-group">
+        <input />
+      </form>
     );
   }
 }
