919ff8872620d3fb3d5666d5fecb9058d63aa377
Nickolas Fuentes <nickfuentes24@gmail.com>
2018-04-16 20:08:17 -0500

    Testing repo name change

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
   render() {
     return (
-      <form className="input-group">
-        <input />
-      </form>
+    
     );
   }
 }
